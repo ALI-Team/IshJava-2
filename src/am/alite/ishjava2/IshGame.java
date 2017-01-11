a571e69475ad318fff6a20b1b5e28a38603e63be
AxelMatstoms <axemat060@edu.linkoping.se>
2017-01-11 14:21:58 +0100

    added some fucking code

--- src/am/alite/ishjava2/IshGame.java
+++ src/am/alite/ishjava2/IshGame.java
@@ -5,15 +5,23 @@
  */
 package am.alite.ishjava2;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
  *
  * @author luka
  */
-public class IshGame {
+public abstract class IshGame {
     private String title;
     private JPanel mainPanel;
+    private JFrame mainFrame;
+    
+    public IshGame() {
+        mainFrame = new JFrame();
+        mainPanel = new JPanel();
+        onCreate();
+    }
     
     public final String getTitle() {
         return title;
@@ -22,4 +30,6 @@ public class IshGame {
     public final void setTitle(String title) {
         this.title = title;
     }
+    
+    abstract void onCreate();
 }
