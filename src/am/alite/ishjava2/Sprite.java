309448bfb7242df01a37d1dc7ecc04be030248d5
Luka Janković <lukjan1999@gmail.com>
2017-01-11 14:31:30 +0100

    sprite class init

---
+++ src/am/alite/ishjava2/Sprite.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package am.alite.ishjava2;
+
+/**
+ *
+ * @author luka
+ */
+public class Sprite  {
+    
+    private int xPos;
+    private int yPos;
+    private String imgPath;
+    
+    public Sprite(String imgPath, int xPos, int yPos)  {
+        
+    }
+    
+}
